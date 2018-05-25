95772e680ed734bdb906a8b88a52166655d5ec32
“Koki <zillion_star_88@yahoo.co.jp>
2018-05-25 21:07:29 +0900

    コメント文追加

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ rl.on('line', (lineString) => {
     const year = parseInt(columns[0]);
     const prefecture = columns[2];
     const popu = parseInt(columns[7]);
-    if (year === 2010 || year === 2015) {
+    if (year === 2010 || year === 2015) {//お試しコメント文
       let value = map.get(prefecture);
               if (!value) {
                   value = {
@@ -28,5 +28,4 @@ rl.on('line', (lineString) => {
 rl.resume();
 rl.on('close', () => {
     console.log(map);
-
 });
